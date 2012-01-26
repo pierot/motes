358fdb787ab0baa0149915e82f2dd9a22eaff131
Pieter Michels <pieter@noort.be>
2012-01-26 21:44:08 +0100

    addng search, delete

--- motes.py
+++ motes.py
@@ -16,7 +16,7 @@ def main():
 
   (options, arguments) = p.parse_args()
 
-  commands = ['create', 'find', 'list', 'open']
+  commands = ['create', 'delete', 'find', 'list', 'open']
 
   if len(arguments) < 1:
     print "Insufficient arguments given."
@@ -31,16 +31,47 @@ def main():
 def exec_command(command, args):
   return {
     'create': exec_create, 
+    'delete': exec_delete, 
     'find': exec_find,
     'list': exec_list, 
     'open': exec_open
   }[command](args)
 
 def exec_create(args):
-  print args
+  filename = args[0] if type(args) == list else args
+  filepath = '/Users/pieterm/Dropbox/Private/notes/' + filename
+
+  print "Mote will create " + filename
+
+  call(['touch', filepath])
+
+  call(['vim', filepath])
+
+def exec_delete(args):
+  filename = args[0] if type(args) == list else args
+  filenr = int(filename) if filename.isdigit() else -1
+ 
+  if filenr > -1:
+    files = glob.glob('/Users/pieterm/Dropbox/Private/notes/*')
+
+    filepath = files[filenr] if len(files) > filenr else ""
+    filename = basename(filepath)
+  else:
+    filepath = '/Users/pieterm/Dropbox/Private/notes/' + filename
+
+  if isfile(filepath):
+    delete_msg = 'Are you sure you want to delete ' + filename + '?'
+    delete_file = yes_no(delete_msg)
+  
+    if delete_file:
+      call(['rm', filepath])
 
 def exec_find(args):
-  print args
+  search = args[0] if type(args) == list else args
+  
+  print 'Motes will search for ' + search
+
+  returncode = call(['ack', '-a', '-i', search, '/Users/pieterm/Dropbox/Private/notes'])
 
 def exec_list(args):
   files = glob.glob('/Users/pieterm/Dropbox/Private/notes/*')
@@ -61,9 +92,19 @@ def exec_open(args):
     filepath = '/Users/pieterm/Dropbox/Private/notes/' + filename
 
     if not isfile(filepath):
-      print "Note does not exist: do you want to create it? (y/n):"
+      make_msg = "Note does not exist: do you want to create it?"
+      make_file = yes_no(make_msg)
+      
+      if make_file:
+        exec_create(filename)
     else:
-      call(['vim', filepath])
+      returncode = call(['vim', filepath])
+
+      if returncode == 0:
+        print 'Mote note closed.'
+
+def yes_no(msg):
+  return True if raw_input("%s (y/n) " % msg).lower() == 'y' else False
 
 # Default init
 if __name__ == '__main__':
