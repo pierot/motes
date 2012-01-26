48617afc0ba13d2ed77c3339715e183654b8abe1
Pieter Michels <pieter@noort.be>
2012-01-26 17:19:18 +0100

    adding command line arguments

---
+++ motes.py
@@ -0,0 +1,47 @@
+#! /user/bin/env python
+
+import sys
+import optparse
+import datetime
+
+def main():
+  usage = "Usage: %prog [command] [options]"
+  p = optparse.OptionParser(usage, version='%prog version 0.1')
+
+  (options, arguments) = p.parse_args()
+
+  commands = ['create', 'find', 'list', 'open']
+
+  if len(arguments) < 1:
+    print "Insufficient arguments given."
+
+    sys.exit(0)
+  
+  if arguments[0] in commands:
+    exec_command(arguments[0], arguments[1::])
+  else:
+    print "Invalid command given, use `" + ', '.join(commands) + "`"
+
+def exec_command(command, args):
+  return {
+    'create': exec_create, 
+    'find': exec_find,
+    'list': exec_list, 
+    'open': exec_open
+  }[command](args)
+
+def exec_create(args):
+  print args
+
+def exec_find(args):
+  print args
+
+def exec_list(args):
+  print args
+
+def exec_open(args):
+  print args
+
+# Default init
+if __name__ == '__main__':
+  main()
