ee445a8e62725780ef714c9ecc595607dd6e7e49
Pieter Michels <pieter@noort.be>
2012-01-27 08:55:18 +0100

    install

--- motes.py
+++ motes.py
@@ -9,6 +9,8 @@ from subprocess import call
 
 from os.path import basename
 from os.path import isfile
+from os.path import exists
+from os import environ
 
 def main():
   usage = "Usage: %prog [command] [options]"
@@ -17,6 +19,9 @@ def main():
   (options, arguments) = p.parse_args()
 
   commands = ['create', 'delete', 'find', 'list', 'open']
+
+  if not motes_home():
+    install_motes()
 
   if len(arguments) < 1:
     print "Insufficient arguments given."
@@ -27,6 +32,48 @@ def main():
     exec_command(arguments[0], arguments[1::])
   else:
     print "Invalid command given, use `" + ', '.join(commands) + "`"
+
+def install_motes():
+  print "Motes wan't installed yet."
+  
+  install_motes_home = yes_no('Install Motes in your home directory (~/.motes)?')
+
+  if install_motes_home:
+    set_motes_home(environ['HOME'] + '/.motes')
+  else:
+    install_motes_path = raw_input("Where do you want Motes to be installed? Please give the full path, no ~. 'Motes' directory will be created: ")
+
+    if len(install_motes_path) > 0 and exists(install_motes_path):
+      install_motes_path = install_motes_path + '/Motes'
+
+      if call(['mkdir', '-p', install_motes_path]) == 0:
+        set_motes_home(install_motes_path)
+      else:
+        print 'Motes install directory could not be created. Permissions problem?'
+
+        sys.exit(0)
+    else:
+      print 'Given path did not exists.'
+
+      sys.exit(0)
+
+def motes_home():
+  try:
+    path_f = open('./.motes', 'r')
+    target = path_f.read()
+    path_f.close()
+
+    return target + '/'
+  except IOError:
+    return False
+
+def set_motes_home(target):
+  try:
+    path_f = open('./.motes', 'w')
+    path_f.writelines(target)
+    path_f.close()
+  except IOError:
+    print 'Error installing motes.'
 
 def exec_command(command, args):
   return {
@@ -39,7 +86,7 @@ def exec_command(command, args):
 
 def exec_create(args):
   filename = args[0] if type(args) == list else args
-  filepath = '/Users/pieterm/Dropbox/Private/notes/' + filename
+  filepath = motes_home() + filename
 
   print "Mote will create " + filename
 
@@ -52,12 +99,12 @@ def exec_delete(args):
   filenr = int(filename) if filename.isdigit() else -1
  
   if filenr > -1:
-    files = glob.glob('/Users/pieterm/Dropbox/Private/notes/*')
+    files = glob.glob(motes_home() + '*')
 
     filepath = files[filenr] if len(files) > filenr else ""
     filename = basename(filepath)
   else:
-    filepath = '/Users/pieterm/Dropbox/Private/notes/' + filename
+    filepath = motes_home() + filename
 
   if isfile(filepath):
     delete_msg = 'Are you sure you want to delete ' + filename + '?'
@@ -71,10 +118,10 @@ def exec_find(args):
   
   print 'Motes will search for ' + search
 
-  returncode = call(['ack', '-a', '-i', search, '/Users/pieterm/Dropbox/Private/notes'])
+  returncode = call(['ack', '-a', '-i', search, motes_home()])
 
 def exec_list(args):
-  files = glob.glob('/Users/pieterm/Dropbox/Private/notes/*')
+  files = glob.glob(motes_home() + '*')
  
   if len(files) > 0:
     print "All motes\n"
@@ -89,7 +136,7 @@ def exec_open(args):
     sys.exit(0)
   else:
     filename = args[0]
-    filepath = '/Users/pieterm/Dropbox/Private/notes/' + filename
+    filepath = motes_home() + filename
 
     if not isfile(filepath):
       make_msg = "Note does not exist: do you want to create it?"
